de3b11b0dc652bbbc5312956acfce7ae08390c65
Victor Miranda <victor.m.diniz@gmail.com>
2016-04-07 00:10:49 +0800

    Update1
    Prototipo jugable

---
+++ Assets/Scripts/DamegeScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamegeScript : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.transform.root != transform.root && col.tag != "Ground" && !col.isTrigger)
+        {
+            if (!col.transform.GetComponent<PlayerControl>().damage) {
+                col.transform.GetComponent<PlayerControl>().damage = true;
+
+                col.transform.root.GetComponentInChildren<Animator>().SetTrigger("Damage");
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/EventHolder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventHolder : MonoBehaviour {
+
+    PlayerControl p1;
+
+	// Use this for initialization
+	void Start () {
+
+        p1 = transform.root.GetComponent<PlayerControl>();
+	}
+    public void ThrowProjectile() {
+        p1.specialAttack = true;
+    }
+
+}
